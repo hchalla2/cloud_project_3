7c4673db4a6f130c655b64950dbaf784f34b2be9
hchalla2 <hchalla2@asu.edu>
2023-04-23 14:51:26 -0700

    Fourth

--- web_tier.py
+++ web_tier.py
@@ -10,26 +10,11 @@ while True:
     response = receive_message(get_request_queue_url())
 
     for message in response.get("Messages", []):
-        message_body = message["Body"]
-        message_dict = json.loads(message_body);
+        lambda_parameters = message["Body"]
         
         delete_message(get_request_queue_url(), message['ReceiptHandle']);
         
-        print(message_dict);
-
-        if message_dict.get("Records") != None:
-            records = message_dict["Records"]
-            for record in records:
-                print(record);
-                print(record["s3"]["object"])
-                video_file_name = record["s3"]["object"]["key"]
-                //invoke lambda
-
-                bucket_name = record["s3"]["bucket"]["name"]
-                
-                lambda_payload = {'bucket_name' : bucket_name, 'file_name' : video_file_name}
-
-                lambda_client.invoke(FunctionName='image-function', InvocationType='Event', Payload=lambda_payload);
+        lambda_client.invoke(FunctionName='image-function', InvocationType='Event', Payload=lambda_parameters);
 
 
 
